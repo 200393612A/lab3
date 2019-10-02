13e4113a09f64eb36d09d284d3559e16e5bad157
yash <200393612@student.georgianc.on.ca>
2019-10-02 17:55:28 -0400

    that's all

---
+++ lab3/routes/Dad.js
@@ -0,0 +1,12 @@
+﻿
+
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('Dad', { title: 'Dad', comment: 'ahhan main baa hu' });
+});
+
+module.exports = router;
---
+++ lab3/routes/index.js
@@ -0,0 +1,11 @@
+﻿
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'YAsh', comment: 'I hate you' });
+});
+
+module.exports = router;
